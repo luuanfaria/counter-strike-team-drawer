a1429ff13b43834a15bc23000a93e611fdff2951
Luan Faria <luuan.fariaf@gmail.com>
2023-10-04 22:55:36 -0300

    Add tailwind configuration, change folder `src/pages` to `src/app` and add `layout.js`.

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <div className="w-full h-screen bg-black flex align-center justify-center">
+      <h1 className="m-auto text-3xl font-bold text-white animate-pulse">
+        CS TEAM DRAWER
+      </h1>
+    </div>
+  );
+}
